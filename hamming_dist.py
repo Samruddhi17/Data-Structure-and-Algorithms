5ada4343940d7c0e805a5b89c3c607e8171f7c22
Samruddhi Naik <samruddhi.naik17@gmail.com>
2018-06-24 01:18:42 -0700

    Find by xor

---
+++ hamming_dist.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        x1 = str(bin(x))[2:]    #convert to binary
+        y1 = str(bin(y))[2:]
+        
+        xr = bin(int(x1,2)^int(y1,2)) #xor to find different bits
+        xrs = str(xrprint(xrs)
+        return xrs.count('1')       #count 1's
