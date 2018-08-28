243f203e6406a252b7ac8c3afdf6a3c03474ba7f
Samruddhi Naik <samruddhi.naik17@gmail.com>
2018-08-28 16:47:11 -0700

    left ,inorder[index] , right

---
+++ Binary Tree/Binary Tree from Preorder and Inorder Traversal/find_preorder_node_inorder(index).py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if inorder:
+            root_index = inorder.index(preorder.pop(0))
+            new_root = TreeNode(inorder[root_index])
+
+            new_root.left = self.buildTree(preorder,inorder[0:root_index])
+            new_root.right = self.buildTree(preorder,inorder[root_index+1:])
+
+            return new_root
